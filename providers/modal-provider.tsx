d1192dbf66493d69172d121870f04e7467b6daf3
Trung DT <thanhtrung.it25@gmail.com>
2024-02-03 16:17:38 +0700

    Product preview modal components (store)

---
+++ providers/modal-provider.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import PreviewModal from '@/components/PreviewModal'
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) {
+    return null
+  }
+  return (
+    <div>
+      <PreviewModal />
+    </div>
+  )
+}
+export default ModalProvider
