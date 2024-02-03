ad269c7fdcbfd19f830bc32611dfd46e2388b8bc
Trung DT <thanhtrung.it25@gmail.com>
2024-02-03 14:35:57 +0700

    Individual category screen (store)

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -15,7 +15,7 @@ const getProducts = async (query: Query): Promise<Product[]> => {
     url: URL,
     query: {
       colorId: query.colorId,
-      sizeId: query.colorId,
+      sizeId: query.sizeId,
       categoryId: query.categoryId,
       isFeatured: query.isFeatured,
     },
