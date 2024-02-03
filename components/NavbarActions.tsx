b17ef656bbada33d56759b4ea71c86c97530e5e1
Trung DT <thanhtrung.it25@gmail.com>
2024-02-03 20:38:30 +0700

    Add Cart functionality

--- components/NavbarActions.tsx
+++ components/NavbarActions.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { ShoppingBag } from 'lucide-react'
 import Button from '@/components/ui/Button'
+import useCart from '@/hooks/use-cart'
 
 const NavbarActions = () => {
   const [isMounted, setIsMounted] = useState(false)
@@ -13,6 +14,7 @@ const NavbarActions = () => {
   }, [])
 
   const router = useRouter()
+  const cart = useCart()
 
   if (!isMounted) {
     return null
@@ -20,9 +22,14 @@ const NavbarActions = () => {
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className=" flex items-center rounded-full bg-black px-4 py-2">
+      <Button
+        onClick={() => router.push('/cart')}
+        className=" flex items-center rounded-full bg-black px-4 py-2"
+      >
         <ShoppingBag size={20} color="white" />
-        <span className="ml-2 text-sm font-medium text-white">2</span>
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   )
--- components/ui/ProductCard.tsx
+++ components/ui/ProductCard.tsx
@@ -8,6 +8,7 @@ import { Expand, ShoppingCart } from 'lucide-react'
 import usePreviewModal from '@/hooks/use-preview-modal'
 import IconButton from '@/components/ui/IconButton'
 import Currency from '@/components/ui/Currency'
+import useCart from '@/hooks/use-cart'
 
 interface ProductCardProps {
   data: Product
@@ -16,6 +17,7 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
   const router = useRouter()
   const previewModal = usePreviewModal()
+  const cart = useCart()
 
   const handleClick = () => {
     router.push(`/product/${data?.id}`)
@@ -25,6 +27,13 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
     event.stopPropagation()
 
     previewModal.onOpen(data)
+  }
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    console.log('onAddToCart')
+    event.stopPropagation()
+
+    cart.addItem(data)
   }
 
   return (
@@ -47,7 +56,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={() => {}}
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
